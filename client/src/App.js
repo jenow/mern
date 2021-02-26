069ea1ec03e43640b930d736854a02e2330d39d8
jenow <jenova.ke20@gmail.com>
2021-02-26 21:52:20 +0100

    handle realtime

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
+const axios = require('axios');
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:3000');
+    socket.on('notify', data => {
+      console.log(data);
+    });
+  }, []);
+
+  function _incrementCount() {
+    setCount(count + 1);
+    axios.get('/posts').then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.err(err);
+    });
+  }
+
   return (
     <div className="App">
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={_incrementCount}>
         Click me
       </button>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const app = express();
+const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const app = express();
 const PORT = 8080;
-const router = require('./src/routes/sample.route');
+
+const io = require('socket.io')(http);
+io.on('connection', function(socket) {
+  // setInterval(() => {
+  //   io.sockets.emit('notify', {foo: 'bar'});
+  // }, 1000);
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+     console.log('A user disconnected');
+  });
+});
+
+const router = require('./src/routes/sample.route')(io);
 
 require('./src/database');
 
-router.use(function(req, res) {
+router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client','build','index.html'));
 });
 
@@ -18,7 +31,7 @@ const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
 app.use(express.static(CLIENT_BUILD_PATH));
 
 // Server React Client
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(CLIENT_BUILD_PATH , "index.html"));
 });
 
@@ -31,6 +44,6 @@ app.use(bodyParser.json());
 
 app.use('/posts', router);
 
-app.listen(PORT, function () {
+http.listen(PORT, () => {
     console.log(`Server Listening on ${PORT}`);
 });
